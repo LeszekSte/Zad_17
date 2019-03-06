898892d10dd84fa1ab58a661626726ec78e27af6
LeszekSte <34620437+LeszekSte@users.noreply.github.com>
2019-03-06 08:55:40 +0100

    Update EulerProject.java
    Wydaje mi się że zadanie 1 będzie szybsze.

--- src/main/java/EulerProject.java
+++ src/main/java/EulerProject.java
@@ -1,5 +1,32 @@
 public class EulerProject {
-    int zadanie1(int brake) {
+    public  int zadanie1(int brake) {
+        int a = 1;
+        int b = 1;
+        int suma = 1;
+        int sumEnd = 0;
+        //   boolean ok = true;
+
+        suma = a + b;
+        b = a;
+        a = suma;
+
+        if (suma % 2 == 0) {
+            sumEnd += suma;
+        }
+
+        suma = 1;
+
+        do {
+            suma = a + b;
+            if (suma % 2 == 0)
+                sumEnd += suma;
+            b = a;
+            a = suma;
+        } while (brake > suma);
+        return sumEnd;
+    }
+
+    int zadanie2(int brake) {
         int sum1 = 1;
         int sum2 = 1;
         int suma = 1;
