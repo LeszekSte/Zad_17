f2164c49e936ceafad39f95663d4c63b88aa5027
LeszekSte <34620437+LeszekSte@users.noreply.github.com>
2019-03-06 08:58:52 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         EulerProject eulerProject = new EulerProject();
-        int zad1 = eulerProject.zadanie1(4000000);
-        System.out.println(zad1);
+     
+        int breake = 4_000_000;
+        int zad1 = eulerProject.zadanie1(breake);
+        int zad2 = eulerProject.zadanie2(breake);
+        
+        System.out.println("Zadanie 1  " + breake+ " - " + zad1);
+        System.out.println("Zadanie 2  " + breake+ " - " + zad2);
     }
 }
